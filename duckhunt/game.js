9b706e9ac73162d31cf9206beff57702566894d3
Steven Manwaring <steven.manwaring@tufts.edu>
2015-10-07 01:23:51 -0400

    added 1st duck

--- duckhunt/game.js
+++ duckhunt/game.js
@@ -2,15 +2,24 @@
 //Comp 20 Lab 5 - Duck Hunt 
 //10/7/15
 
+
 function init(){
 	canvas = document.getElementById('game_canvas');
 	ctx = canvas.getContext('2d');
 
-	var img = new Image(); 
-	img.onload = function() {
-		ctx.drawImage(img, 0, 0) 
+	//load background
+	var background_img = new Image(); 
+	background_img.onload = function(){
+		ctx.drawImage(background_img, 0, 0); 
+	} 
+	background_img.src = 'duckhunt-background.gif';
+	
+	//load first duck
+	var duck_1 = new Image();
+	duck_1.onload = function(){
+		ctx.drawImage(duck_1, 38, 120, 35, 35, 70, 65, 35, 35); 
 	}
-
-
-	img.src = 'duckhunt-background.gif';
+	duck_1.src = 'duckhunt_various_sheet.png';
+	//load second duck
 }
+
