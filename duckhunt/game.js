98ed5cb7ffda062b769b13188cf6c85ccb850010
Steven Manwaring <steven.manwaring@tufts.edu>
2015-10-07 00:48:44 -0400

    Got background image to display

--- duckhunt/game.js
+++ duckhunt/game.js
@@ -1,1 +1,16 @@
-// Replace this with your code...
+// Steven Manwaring
+//Comp 20 Lab 5 - Duck Hunt 
+//10/7/15
+
+function init(){
+	canvas = document.getElementById('game_canvas');
+	ctx = canvas.getContext('2d');
+
+	var img = new Image(); 
+	img.onload = function() {
+		ctx.drawImage(img, 0, 0) 
+	}
+
+
+	img.src = 'duckhunt-background.gif';
+}
